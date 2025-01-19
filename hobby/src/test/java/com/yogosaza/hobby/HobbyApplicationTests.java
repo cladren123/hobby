5f7ca2f370b0fe59f80a323dfeda32c77070bcd7
KimDongEon <gold3987@naver.com>
2025-01-19 17:48:28 +0900

    [feat] 프로젝트 생성

---
+++ hobby/src/test/java/com/yogosaza/hobby/HobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogosaza.hobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
