e377d5e486d789f5d1b1425c8f5e2bd92af850de
KimDongEon <gold3987@naver.com>
2025-01-20 00:06:19 +0900

    [feat] 2025-01-20 12:06 order controller registerorder 구현

--- hobby/src/main/java/com/yogosaza/hobby/controller/OrderController.java
+++ hobby/src/main/java/com/yogosaza/hobby/controller/OrderController.java
@@ -1,6 +1,9 @@
 package com.yogosaza.hobby.controller;
 
+import com.yogosaza.hobby.dto.OrderRegisterDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 public class OrderController {
 
-  
+  @PostMapping("")
+  public ResponseEntity<?> registerOrder(@RequestBody OrderRegisterDto dto) {
+    return ResponseEntity.ok("ok");
+  }
 
 }
